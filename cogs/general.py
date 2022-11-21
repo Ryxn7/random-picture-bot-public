63db49f5f79955f1112aa1cf521c67831720d1ee
Ryan Chen <94326100+Ryxn7@users.noreply.github.com>
2022-11-21 00:26:18 -0500

    v1.9

--- cogs/general.py
+++ cogs/general.py
@@ -18,26 +18,26 @@ class General(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         invite_link = database["invite_link"]
-        activity = discord.Game(name = f"b.help | {invite_link}")
+        activity = discord.Game(name = f"/help | {invite_link}")
         await self.client.change_presence(status=discord.Status.online, activity=activity)
         logger.info("Random Picture Bot is ready.")
 
 
-    @commands.command()
+    @discord.slash_command(name='hi')
     async def hi(self, ctx):
         if ctx.author.id == 367469641150365702:
-            await ctx.send('Heya!')
+            await ctx.respond('Heya!')
     
 
     # Returns the User's ping
-    @commands.command()
+    @discord.slash_command(name='ping')
     async def ping(self, ctx):
         ping = int(round(self.client.latency * 1000))
-        await ctx.send(f"Pong! Your ping is {ping}ms")
+        await ctx.respond(f"Pong! Your ping is {ping}ms")
 
 
     # Returns information on how to use the bot        
-    @commands.command(aliases=['h'])
+    @discord.slash_command(name='help', description='Help Prompt for the Bot')
     async def help(self, ctx):
 
         embed = discord.Embed(
@@ -54,15 +54,13 @@ class General(commands.Cog):
 
         embed.add_field(
             name = "Commands List",
-            value = "`b.commands` > Returns the list of Random Pictures Bot's commands!" \
-                    "\nAliases: `b.command`, `b.c`",
+            value = "`/commands` > Returns the list of Random Pictures Bot's commands!",
             inline = False
         )
 
         embed.add_field(
             name = "Bot Invite Link",
-            value = "`b.invite` > Returns the bot's invite link" \
-                    "\nAliases: `b.i`",
+            value = "`/invite` > Returns the bot's invite link",
             inline = False
         )
         
@@ -70,21 +68,22 @@ class General(commands.Cog):
         embed.set_thumbnail(url=image)
         embed.set_footer(text="By Ryxn <3")
 
-        await ctx.send(embed=embed)
+        await ctx.respond(embed=embed)
     
 
     # Returns the invite link for the Bot
-    @commands.command(aliases=['i'])
+    @discord.slash_command(name='invite', description="Random Picture Bot's Invite link")
     async def invite(self, ctx):
 
         invite_link = database["invite_link"]
         message = "Invite Random Picture Bot to your own server using this link!" \
                   f"\n> {invite_link}"
-        await ctx.send(message)
+                  
+        await ctx.respond(message)
 
 
     # Returns a list of commands that offered by the Bot
-    @commands.command(name='commands', aliases=['command', 'c'])
+    @discord.slash_command(name='commands', description='Commands List')
     async def _commands(self, ctx):
 
         embed = discord.Embed(
@@ -93,69 +92,62 @@ class General(commands.Cog):
             )
 
         embed.add_field(
-            name = "***b.help***",
-            value = "> Returns the help prompt." \
-                    "\n> `(Aliases: b.h)`",
+            name = "***/help***",
+            value = "> Returns the Bot's help prompt.",
             inline = False
         )
 
         embed.add_field(
-            name = "***b.randompic (pic_req)***",
-            value = "> Returns the a random picture of your request!" \
-                    "\n> `(Aliases: b.randpic, b.rp)`" \
-                    "\n> `Ex: b.randompic dog`",
+            name = "***/randompic (pic_req)***",
+            value = "> Returns a random picture of your request!"
+                    "\n> `Ex: /randompic dog`",
             inline = False
         )
 
         embed.add_field(
-            name = "***b.add (pic_name) (link)***",
-            value = "> Use this command to add pictures to the bot's online database!" \
-                    "\n> `(Aliases: b.a)`" \
-                    "\n> `Ex: b.add vega65 (image_link)`",
+            name = "***/add (pic_name) (link)***",
+            value = "> Use this command to add pictures to the bot's online database!"
+                    "\n> `Ex: /add vega65 (image_link)`",
             inline = False
         )
 
         embed.add_field(
-            name = "***b.remove (pic_name) (link)***",
-            value = "> Use this command to remove a picture from the bot's online database!" \
-                    "\n> `(Aliases:  b.r)`" \
-                    "\n> `Ex: b.remove vega65 (image_link)`",
+            name = "***/remove (pic_name) (link)***",
+            value = "> Use this command to remove a picture from the bot's online database!"
+                    "\n> `Ex: /remove vega65 (image_link)`",
             inline = False
         )
 
         embed.add_field(
-            name = "***b.list***",
-            value = "> Returns the list of pictures on the bot's online database!" \
-                    "\n> `(Aliases: b.l)`",
+            name = "***/list***",
+            value = "> Returns the list of pictures on the bot's online database!",
             inline = False
         )
 
         embed.add_field(
-            name = "***b.picture (pic_name)***",
-            value = "> Returns the requested picture from the bot's online database!" \
-                    "\n> `(Aliases: b.p)`" \
-                    "\n> `Ex: b.picture vega65`",
+            name = "***/picture (pic_name)***",
+            value = "> Returns the requested picture from the bot's online database!"
+                    "\n> `Ex: /picture vega65`",
             inline = False
         )
 
         embed.add_field(
-            name = "***b.invite***",
-            value = "> Returns the invite link for Random Picture Bot!" \
-                    "\n> `(Aliases: b.i)`",
+            name = "***/invite***",
+            value = "> Returns the invite link for Random Picture Bot!",
             inline = False
         )
 
         embed.add_field(
-            name = "***b.ping***",
+            name = "***/ping***",
             value = "> Returns your ping!",
             inline = False
         )
 
-        await ctx.send(embed=embed)
+        await ctx.respond(embed=embed)
 
 
     # Command to push changes made to pictures.json to Github
-    @commands.command()
+    @discord.slash_command(name='gitpush', description="Manually push new data to the Bot's online database")
     async def gitPush(self, ctx):
         if ctx.author.id == 367469641150365702:
             filenames = ["data/pictures.json"]
@@ -189,7 +181,7 @@ class General(commands.Cog):
                         print("Nothing to update.")
                 except Exception as e:
                     logger.exception(e)
-            await ctx.send("Pushed latest data to GitHub.")
+            await ctx.respond("Pushed latest data to GitHub.")
 
 
 def setup(client):
